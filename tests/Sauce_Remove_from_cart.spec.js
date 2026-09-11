4107abeecbac28cbca4a683145a69e01b5c97318
kamala <kamalavenkat21@gmail.com>
2026-09-11 18:32:24 -0400

    Renaming the file name - Adding to cart

--- tests/Sauce_Remove_from_cart.spec.js
+++ tests/Sauce_Remove_from_cart.spec.js
@@ -1,23 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { sauce_POM } from '../page/sauce_POM';
 
-test('add Sauce Labs Fleece Jacket to the cart', async ({ page }) => {
-    const login = new sauce_POM(page);
-
-    await login.gotologinpage();
-    await login.login('visual_user', 'secret_sauce');
-
-    const fleeceJacket = page.locator('.inventory_item').filter({
-        hasText: 'Sauce Labs Fleece Jacket'
-    });
-
-    await expect(fleeceJacket).toBeVisible();
-    await fleeceJacket.getByRole('button', { name: 'Add to cart' }).click();
-
-    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
-});
-
-test.only('remove Sauce Labs Fleece Jacket from the cart', async ({ page }) => {
+test('remove Sauce Labs Fleece Jacket from the cart', async ({ page }) => {
     const login = new sauce_POM(page);
 
     await login.gotologinpage();
